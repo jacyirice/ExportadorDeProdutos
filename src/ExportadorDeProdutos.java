58809f8acfee20c27619723a316cf9dde0aae122
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2022-04-11 10:03:27 -0300

    Adicionando arquivos da aula

---
+++ src/ExportadorDeProdutos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.jacyirice.pp.exportador.ExportadorListaProduto;
+import com.jacyirice.pp.exportador.Produto;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author compo
+ */
+public class ExportadorDeProdutos {
+
+    public static void main(String[] args) {
+        final List<Produto> produtos = new ArrayList<>();
+        produtos.add(new Produto("TV", "LG", "132-A", 120));
+        produtos.add(new Produto("Notebook", "Asus", "New age", 341));
+        produtos.add(new Produto("Smartphone", "Samsung", "Galaxy S10", 214));
+
+        ExportadorListaProduto exportadorPadrao = ExportadorListaProduto.newInstance();
+        System.out.println("Lista de Produtos em HTML\n");
+        System.out.println(exportadorPadrao.exportar(produtos));
+
+        ExportadorListaProduto exportadorMarkdown = ExportadorListaProduto.newInstance("md");
+        System.out.println("Lista de Produtos em Markdown\n");
+        System.out.println(exportadorMarkdown.exportar(produtos));
+    }
+
+}
